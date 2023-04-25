e2142f9baa2652f9c90c874ebbf43dc78c800d17
antoniuv <115102470+antoniuv@users.noreply.github.com>
2023-04-26 01:50:13 +0300

    Create tema2.cpp

---
+++ tema2/tema2.cpp
@@ -0,0 +1,10 @@
+#include "agentie.h"
+
+int main() {
+    AgentieImobiliara agentie;
+    std::cout << "Introduceti datele locuintelor (numarul de locuinte, apoi datele pentru fiecare locuinta):"<<std::endl;
+    std::cin >> agentie;
+    std::cout << agentie;
+
+return 0;
+}
